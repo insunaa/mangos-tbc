4b6ac4e5d0b08c1f1d1d6cdaef33317ce98a185c
insunaa <dev.insuna@gmail.com>
2023-02-02 18:30:30 +0100

    Add declarations in header

--- src/game/Entities/Unit.h
+++ src/game/Entities/Unit.h
@@ -2442,6 +2442,11 @@ class Unit : public WorldObject
 
         virtual bool IsNoWeaponSkillGain() const { return false; }
 
+        void SendThreatUpdate();
+        void SendHighestThreatUpdate(HostileReference* pHostilReference);
+        void SendThreatClear() const;
+        void SendThreatRemove(HostileReference* pHostileReference) const;
+
     protected:
         bool MeetsSelectAttackingRequirement(Unit* target, SpellEntry const* spellInfo, uint32 selectFlags, SelectAttackingTargetParams params) const;
 
